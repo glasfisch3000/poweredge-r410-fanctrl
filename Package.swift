d7904371fc57cba5838cf2fb23ac9c69012b577e
glasfisch3000 <jakob@danckwerts.net>
2024-08-13 16:36:19 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "poweredge-r410-fanctrl",
+    platforms: [
+        .macOS(.v13),
+    ],
+    dependencies: [
+    ]
+)
